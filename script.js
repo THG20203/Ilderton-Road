6522bf494498abcbb274ba5e53f0d46c0fa8eacb
THG20203 <tristanhgriffiths8@yahoo.com>
2024-03-05 12:12:07 +0000

    got parralax working

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 let zampaBackground = document.querySelector(".zampa-background");
 let stadiumDrawing = document.querySelector(".stadium-background");
+let ildertonRoad = document.querySelector(".ilderton-road");
 
 window.addEventListener("scroll", () => {
   const zampaScrollPosition = window.scrollY;
-  const zampaScale = 1 + zampaScrollPosition / 1000;
+  const zampaScale = 1 + zampaScrollPosition / 600;
   zampaBackground.style.transform = `scale3d(${zampaScale}, ${zampaScale}, 1)`;
-});
 
-window.addEventListener("scroll", () => {
   const stadiumScrollPosition = window.scrollY;
-  const scale = 1 + stadiumScrollPosition / 900;
+  const scale = 1 + stadiumScrollPosition / 1000;
   stadiumDrawing.style.transform = `scale3d(${scale}, ${scale}, 1)`;
 });
