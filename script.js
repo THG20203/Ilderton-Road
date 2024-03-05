16d6b43a206793939f5a8f470be72d2df6beb00f
THG20203 <tristanhgriffiths8@yahoo.com>
2024-03-05 09:55:26 +0000

    starting looking into css

--- script.js
+++ script.js
@@ -3,16 +3,12 @@ let stadiumDrawing = document.querySelector(".stadium-background");
 
 window.addEventListener("scroll", () => {
   const zampaScrollPosition = window.scrollY;
-  const zampaScale = 1 + zampaScrollPosition / 200;
+  const zampaScale = 1 + zampaScrollPosition / 1000;
   zampaBackground.style.transform = `scale3d(${zampaScale}, ${zampaScale}, 1)`;
 });
 
 window.addEventListener("scroll", () => {
   const stadiumScrollPosition = window.scrollY;
-  const scale = 1 + stadiumScrollPosition / 1000;
+  const scale = 1 + stadiumScrollPosition / 900;
   stadiumDrawing.style.transform = `scale3d(${scale}, ${scale}, 1)`;
-
-  const backgroundSizePercentage = 50 + stadiumScrollPosition / 5; // Adjust the rate of increase as needed
-  stadiumDrawing.style.backgroundSize = `${backgroundSizePercentage}%`;
-  stadiumDrawing.style.height = `${80 + stadiumScrollPosition / 10}vh`; // Adjust the rate of increase as needed
 });
