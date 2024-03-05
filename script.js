b8cdc959968ba61cd146612f4b1752617a3f8da8
THG20203 <tristanhgriffiths8@yahoo.com>
2024-03-05 14:12:30 +0000

    parralax sorted on desktop

--- script.js
+++ script.js
@@ -3,16 +3,20 @@ let stadiumDrawing = document.querySelector(".stadium-background");
 
 window.addEventListener("scroll", () => {
   const zampaScrollPosition = window.scrollY;
-  const zampaScale = 1 + zampaScrollPosition / 600;
+  const zampaScale = 1 + zampaScrollPosition / 800;
   zampaBackground.style.transform = `scale3d(${zampaScale}, ${zampaScale}, 1)`;
 
-  // Check if the scale of zampaBackground reaches 2
-  if (zampaScale >= 2) {
-    // Set opacity to 0 for both zampaBackground and stadiumDrawing
+  // Check if the scale of zampaBackground reaches 1.5
+  if (zampaScale >= 1.3) {
+    // Set opacity to 0 for both zampaBackground and stadiumDrawing with transition
+    zampaBackground.style.transition = "opacity 0.5s ease";
+    stadiumDrawing.style.transition = "opacity 0.5s ease";
     zampaBackground.style.opacity = 0;
     stadiumDrawing.style.opacity = 0;
   } else {
-    // Reset opacity if zampaScale is less than 2
+    // Reset opacity if zampaScale is less than 1.5
+    zampaBackground.style.transition = "opacity 0.5s ease";
+    stadiumDrawing.style.transition = "opacity 0.5s ease";
     zampaBackground.style.opacity = 1;
     stadiumDrawing.style.opacity = 1;
   }
