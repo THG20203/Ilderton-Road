495c3914cf31699d38375191ef6a1f835535bf9f
THG20203 <tristanhgriffiths8@yahoo.com>
2024-03-06 00:20:49 +0000

    made parallax javascript much more dry

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 let zampaBackground = document.querySelector(".zampa-background");
 let stadiumDrawing = document.querySelector(".stadium-background");
 
+//PARALLAX CODE
 window.addEventListener("scroll", () => {
+  //PARALLAX VARIABLES
   const zampaScrollPosition = window.scrollY;
-  const zampaScale = 1 + zampaScrollPosition / 800;
-  zampaBackground.style.transform = `scale3d(${zampaScale}, ${zampaScale}, 1)`;
-
-  // Check if the scale of zampaBackground reaches 1.5
-  if (zampaScale >= 1.3) {
-    // Set opacity to 0 for both zampaBackground and stadiumDrawing with transition
-    zampaBackground.style.transition = "opacity 0.5s ease";
-    stadiumDrawing.style.transition = "opacity 0.5s ease";
-    zampaBackground.style.opacity = 0;
-    stadiumDrawing.style.opacity = 0;
-  } else {
-    // Reset opacity if zampaScale is less than 1.5
-    zampaBackground.style.transition = "opacity 0.5s ease";
-    stadiumDrawing.style.transition = "opacity 0.5s ease";
-    zampaBackground.style.opacity = 1;
-    stadiumDrawing.style.opacity = 1;
-  }
-
   const stadiumScrollPosition = window.scrollY;
-  const scale = 1 + stadiumScrollPosition / 1000;
-  stadiumDrawing.style.transform = `scale3d(${scale}, ${scale}, 1)`;
+
+  // PARALLAX FUNCTIONS
+  const calculateScale = (scrollPosition, divisionFactor) => {
+    /* 1 + to make sure it doesn't go to 0 or negative and flip it */
+    /* divisianFactor is arbitary number for scaling, lower number dividing by faster scaling */
+    return 1 + scrollPosition / divisionFactor;
+  };
+
+  /* once got scale from calculateScale can use it as scale second argument when calling function */
+  const applyTransform = (element, scale) => {
+    element.style.transform = `scale3d(${scale}, ${scale}, 1)`;
+  };
+
+  const applyOpacity = (element, scale) => {
+    element.style.transition = "opacity 0.5s ease";
+    /* if scale is greater or equal  than 1.3 opacity 0 if not reached that yet -> 1 */
+    element.style.opacity = scale >= 1.3 ? 0 : 1;
+  };
+
+  const zampaScale = calculateScale(zampaScrollPosition, 800);
+  const stadiumScale = calculateScale(stadiumScrollPosition, 1000);
+  applyTransform(zampaBackground, zampaScale);
+  applyOpacity(zampaBackground, zampaScale);
+  applyTransform(stadiumDrawing, stadiumScale);
+  applyOpacity(stadiumDrawing, stadiumScale);
 });
