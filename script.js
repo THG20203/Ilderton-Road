e2a45f861c89d39f4b98f8d49bbd0513ec8560f1
THG20203 <tristanhgriffiths8@yahoo.com>
2024-03-18 23:41:02 +0000

    play and pause functionality added

--- script.js
+++ script.js
@@ -95,6 +95,38 @@ postcodeButton.addEventListener("click", () => {
 });
 
 /* VIDEO PLAYER FUNCTIONALITY */
+/* Play and pause video */
+const toggleVideoStatus = () => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+/* Update play/pause icon */
+const updatePlayIcon = () => {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+};
+
+/* Update the progress and timestamp */
+const updateProgress = () => {
+  return true;
+};
+
+const setVideoProgress = () => {
+  return true;
+};
+
+const stopVideo = () => {
+  return true;
+};
+
+// VIDEO EVENT LISTENERS
 video.addEventListener("click", toggleVideoStatus);
 video.addEventListener("pause", updatePlayIcon);
 video.addEventListener("play", updatePlayIcon);
